630a7ce3f9224405a665bd4f3b176b3b2ce02f2d
PolyET <78802945+PolyET@users.noreply.github.com>
2023-07-12 16:50:29 +0200

    Update Greeter.java

--- server/src/main/java/com/example/Greeter.java
+++ server/src/main/java/com/example/Greeter.java
@@ -22,7 +22,10 @@ public class Greeter {
   public void fct01() { 
   }
   
- 
+  /**
+ * @param someone name of personn
+ * @return greeting string
+ */
   public final String greet(final String someone) {
     return String.format("Hello babacar, %s!", someone);
    
